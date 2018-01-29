660d24b4457db457daa4f01899230a56e0cc9ab2
szaharici <simeon.zaharici@gmail.com>
2018-01-29 17:47:09 -0500

    removing stuff

--- collectors/etc/aws_cloudwatch_conf.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-def enabled():
-  return True
-
-def get_accesskey_secretkey():
-  return ('<access_key_id>', '<secret_access_key>')
--- collectors/etc/smart_stats_conf.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-def get_config():
-
-    config = {
-        'collection_interval': 120    # Seconds, how often to collect metric data
-    }
-
-    return config
